72a1e063b645a8dc8651ba8ae1af0ce765833e37
zhuangxincheng <18310763990@163.com>
2019-11-15 16:15:47 +0800

    完善

--- src/test/java/com/example/websocket/WebsocketApplicationTests.java
+++ src/test/java/com/example/websocket/WebsocketApplicationTests.java
@@ -1,16 +1,17 @@
 package com.example.websocket;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.math.BigDecimal;
+
+
 public class WebsocketApplicationTests {
 
     @Test
     public void contextLoads() {
+        BigDecimal a =new BigDecimal(String.valueOf(2)).subtract(new BigDecimal(String.valueOf(1.1)));
+        //BigDecimal c = new BigDecimal(a);
+        System.out.println("!!!!!!!!!!!!!!!"+a);
     }
 
 }
